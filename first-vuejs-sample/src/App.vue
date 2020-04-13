e10f6e92c1f92e53cc06b7ab2777974b597745a4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-13 13:38:55 +0530

    Few Changes

---
+++ first-vuejs-sample/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App. I am developing for the first time."/>
+    <First msg="First Component"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import First from './components/First.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    First
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ first-vuejs-sample/src/components/First.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="demo">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "First",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.demo {
+  background-color: rgb(203, 247, 177);
+}
+</style>
